23fb90a0f7513544387e31baf4714f0a001369aa
Samuel López Saura <samuellopezsaura@gmail.com>
2019-07-04 22:19:09 +0200

    Add EntryPointNotEmpty and LevelNotRegistered core exceptions

--- backend/core/exceptions.py
+++ backend/core/exceptions.py
@@ -1,11 +1,24 @@
-
 class AlreadyRegisteredLevel(Exception):
     """Exception to be raised when a Level identifier collision appears
     in history at register time"""
+
     pass
 
 
 class EntrypointDoesNotExist(Exception):
     """Exception to be raised when a queried entrypoint does not exist in
-    history"""
+    story"""
+
     pass
+
+
+class EntrypointNotEmpty(Exception):
+    """Exception to be raised when a Level try to be registered
+    in a Entrypoint which has already a registered level"""
+
+    pass
+
+
+class LevelNotRegistered(Exception):
+    """Exception to be raised when a Story try to get access to a
+    level not registered in it"""
