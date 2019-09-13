5ea186568a94039809e67302916af5c70e73f396
Samuel López Saura <samuellopezsaura@gmail.com>
2019-09-13 15:28:28 +0200

    First Laboratory Access Layer definition idea

---
+++ backend/core/lal/connectors/interfaces.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+from . import Status
+
+
+class AccessInformation(ABC):
+    """Information needed to access to a Lab by a client"""
+    pass
+
+
+class Connection(ABC):
+
+    @abstractmethod
+    def open(self) -> bool:
+        """Initializes a connection with a Lab provider.
+        Return True if no errors are provided.
+        """
+        pass
+
+    @abstractmethod
+    def status(self) -> Status:
+        """Return the lab status.
+       eg: Status.LAB_INITIALIZED"""
+        pass
+
+    @abstractmethod
+    def close(self):
+        """TearDown the laboratory."""
+        pass
