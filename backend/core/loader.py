8de3559b4ca602c7793cad89e8338c89e518722a
Samuel López Saura <samuellopezsaura@gmail.com>
2019-09-25 23:33:30 +0200

    Initial steps in levels packages installation.

--- backend/core/loader.py
+++ backend/core/loader.py
@@ -1,7 +1,10 @@
 from os import listdir
-from os import path
-from typing import List
+from os import path, mkdir
 import json
+import shutil
+
+import tarfile
+from typing import List
 
 from .game import Level, Entrypoint
 from .types import LevelIdentifier, EntrypointCodename
@@ -17,6 +20,8 @@ class Loader:
     # TODO: Doc this class
 
     LEVELS_PATH = "backend/levels"
+    LEVELS_UI = "ui/src/levels"
+    TEMP = '/tmp'
     MINIMUM_LEVEL_FILES = ("manifest.json", "level.py")
     MINIMUM_MANIFEST_KEYS = ("id", "startpointLevelId", "startpointCodename", "entrypoints")
 
@@ -51,6 +56,69 @@ class Loader:
             )
             ids_parsed.append(level_manifest["id"])
             yield level
+
+    def __move_to(origin_folder: str, dst_folder: str):
+        """Move all the files and folders inside a origin to a destination.
+
+        :param origin_folder: Folder with files and directories inside to move.
+        :type origin_folder: str
+
+        :param dst_folder: Folder to receive the moved files and directories.
+        :type dst_folder: str
+        """
+
+        for file_or_folder in listdir(origin_folder):
+            shutil.move(path.join(origin_folder, file_or_folder), dst_folder)
+
+    @classmethod
+    def install_new_level(self, level_filename: str):
+        """Load a Level from a .tar.gz file.
+
+        Raises a FileNotFoundError if the level_filename provided does'nt exist
+        Raises a InvalidManifestStructure if the manifest is invalid.
+
+        :param level_filename: .tar.gz file with the level structure
+        :type level_filename: str
+        """
+
+        assert level_filename.endswith('.level'), ('The provided file is not '
+                                                   'a valid level')
+
+        level_name = path.basename(level_filename).split('.')[0]
+
+        with tarfile.open(level_filename, 'r:gz') as level_tar:
+            level_tar.extractall(path=path.join(self.TEMP, level_name))
+
+        level_tmp_folder = path.join(self.TEMP, level_name)
+
+        manifest_file = path.join(level_tmp_folder, 'manifest.json')
+
+        with open(manifest_file) as manifest:
+            level_manifest = json.loads(manifest.read())
+
+        self.check_valid_level_manifest(level_manifest)
+
+        level_backend_path = path.join(self.LEVELS_PATH, level_manifest['id'])
+        level_frontend_path = path.join(self.LEVELS_UI, level_manifest['id'])
+
+        # TODO check not in local levels
+
+        # Create backend structure
+        mkdir(level_backend_path)
+
+        # Create frontend structure
+        mkdir(level_frontend_path)
+
+        # Move files into level folder
+        shutil.move(manifest_file, level_backend_path)
+        self.__move_to(path.join(level_tmp_folder, 'backend'),
+                       level_backend_path)
+
+        self.__move_to(path.join(level_tmp_folder, 'frontend'),
+                       level_frontend_path)
+
+        # Remove temporary folder
+        shutil.rmtree(level_tmp_folder)
 
     @classmethod
     def check_valid_level_manifest(cls, level_manifest: dict):
