677e61e306f4bd6df3a494a0185e186c66d2900c
Samuel López Saura <samuellopezsaura@gmail.com>
2020-02-20 23:12:14 +0100

    Add dummy configuration module to avoid getting coupled to any framework configuration

---
+++ config.py
@@ -0,0 +1,11 @@
+from django.conf import settings as conf
+
+"""Empty configuration file just to be loosly coupled to django or other
+framework configuration.
+
+Other apps can get configuration elements importing conf variable name from
+this module.
+
+eg. `from core.config import conf`
+
+"""
