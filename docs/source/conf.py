d1e631c97c8fddf28246dcc6e5fd6e36ff82f6f7
Samuel López Saura <samuellopezsaura@gmail.com>
2020-03-25 23:39:31 +0100

    Update copyright notice and rtd theme to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Reality Mismatch'
-copyright = '2019, Samuel Lopez Saura'
-author = 'Samuel Lopez Saura'
+copyright = '2020, Samuel López Saura'
+author = 'Samuel López Saura'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
@@ -47,9 +47,10 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_show_sourcelink = False
