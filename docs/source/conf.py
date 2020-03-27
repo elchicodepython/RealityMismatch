8c4e87d72cbdf221c3497839136ce4cc89dd4431
Samuel López Saura <samuellopezsaura@gmail.com>
2020-03-27 23:57:38 +0100

    Update release version to alpha

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Samuel López Saura'
 author = 'Samuel López Saura'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.1-alpha'
 
 
 # -- General configuration ---------------------------------------------------
